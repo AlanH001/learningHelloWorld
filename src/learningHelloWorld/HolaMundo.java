66be5a32ba8531a6d0735eb8606311070ac66a70
Walter <walterfrancoy@gmail.com>
2018-08-21 20:15:45 -0300

    Branch

--- src/learningHelloWorld/HolaMundo.java
+++ src/learningHelloWorld/HolaMundo.java
@@ -2,8 +2,10 @@ package learningHelloWorld;
 
 public class HolaMundo {
 	public static void main (String [] args) {
-		System.out.println ("Hola puto");
+		System.out.println ("Hola mundirijilli");
 
 		System.out.println("editando codigo desde gitkraken");
+
+		System.out.println("creando branch");
 	}
 }
