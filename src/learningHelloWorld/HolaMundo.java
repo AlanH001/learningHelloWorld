23fb60eaedef06431ce4819930d4c192151eb7ba
Walter <walterfrancoy@gmail.com>
2018-08-21 20:06:08 -0300

    Gitkraken
    asdasdf

--- src/learningHelloWorld/HolaMundo.java
+++ src/learningHelloWorld/HolaMundo.java
@@ -3,5 +3,7 @@ package learningHelloWorld;
 public class HolaMundo {
 	public static void main (String [] args) {
 		System.out.println ("Hola puto");
+
+		System.out.println("editando codigo desde gitkraken");
 	}
 }
